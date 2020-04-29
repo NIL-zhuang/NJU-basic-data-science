116b313dcbf43a2fad95e701bab030c8f862abf3
Cheng Rongxin <rongxinCRX@outlook.com>
2020-04-29 20:47:12 +0800

    feat: committed again

--- evaluator.py
+++ evaluator.py
@@ -38,32 +38,40 @@ class ScoreEvaluator:
     # TODO 用来判断是否面向用例
     def ischeated(cls, file):
         test_cases = cls.workdir + '/resource/.mooctest/testCases.json'
-        all_the_code = open(file).read()
+        all_the_code = open(file).read().split('\n')
         with open (test_cases, 'r') as f:
             cls.cases = json.load(f)
         for case in cls.cases:
             output = case["output"]
-            output = output.replace('\n', '')
-            if output in all_the_code: return True  # 这边的代码逻辑有待商榷 到底是只要匹配到一个就算作弊 还是匹配到所有才算作弊
+            output = output.replace('\n', '').split()
+            for code in all_the_code:
+                code = code.replace('#.*', '')
+                if code != '' and 'return' not in code:
+                    times = 0
+                    for out in output:
+                        if out in code: times += 1
+                    if times == len(output): return True  # 匹配成功
+            # 这边的代码逻辑有待商榷 到底是只要匹配到一个用例就算作弊 还是匹配到所有才算作弊
         return False
 
     @classmethod
-    def getscore(cls, url):
-        file = cls.read_from_url(url)
+    def getscore(cls, code_url, recycle=5):
+        file = cls.read_from_url(code_url)
         if not file: return -1  # cpp提交
         if cls.ischeated(file): return 0  # 面向用例
 
         runtime = 0
-        for case in cls.cases:
-            input = case["input"] + '\n'
-            test = open(cls.workdir+'/test.txt', 'w')
-            test.write(input)
-            test.close()
-            timestamp_start = time.time() * 1000
-            os.system('python3 {}<{}>>{}'.format(file, cls.workdir+'/test.txt', cls.workdir+'/test.txt'))  # 与真实的运行时间有略微差异，因为是调用os模块从命令行调用的
-            timestamp_end = time.time() * 1000
-            runtime += timestamp_end - timestamp_start
-
+        for i in range(recycle):
+            for case in cls.cases:
+                input = case["input"] + '\n'
+                test = open(cls.workdir+'/test.txt', 'w')
+                test.write(input)
+                test.close()
+                timestamp_start = time.time() * 1000
+                os.system('python3 {}<{}>>{}'.format(file, cls.workdir+'/test.txt', cls.workdir+'/test.txt'))  # 与真实的运行时间有略微差异，因为是调用os模块从命令行调用的
+                timestamp_end = time.time() * 1000
+                runtime += timestamp_end - timestamp_start
+        runtime /= recycle  # 通过取平均值尽量减少os.system带来的时间波动误差，如果对次数不满意可以自己传入recycle参数
         # 确保配置了python的环境变量
         # window环境下将上面的"python3"修改成"python"
 
