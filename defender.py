4cd303afbe9f4a8905fad3e1548f13267817804f
Cheng Rongxin <rongxinCRX@outlook.com>
2020-07-01 16:31:11 +0800

    refactor: 重构了代码，强化了匹配性能

---
+++ defender.py
@@ -0,0 +1,42 @@
+import re
+
+
+class Defender:
+    @classmethod
+    # 检查是否为cpp提交
+    # 原理很简单，用运行cpp程序必不可少的int main或者void main判断即可
+    def cppDefend(cls, code):
+        for line in code:
+            if 'int main' in line or 'void main' in line: return True
+        return False
+
+    @classmethod
+    # separator: 输出结果的分割符号
+    # codes: 源代码
+    # cases: 用例集，每个元素包含输入和输出
+    def cheatDefend(cls, separator, codes, cases):
+        cheats = 0
+        for case in cases:
+            case_input = case['input']
+            output = case['output']
+            # 逐行代码解释
+            for line in codes:
+                code = re.sub(r'#.*$', "", line)  # 忽略代码中的注释
+                if code == '': continue
+
+                times = 0  # 分段匹配命中次数
+                # 暂时需要排除true or false，这个凭借输出不好判断，详见else逻辑块
+                if output != 'True' and output != 'False' and output != 'true' and output != 'false':
+                    standard = output.replace('\n', '').split(separator)  # 输出不一定是一个值，有可能是一组
+                    for out in standard:
+                        if out != '' and out in code: times += 1
+                else:
+                    standard = case_input.replace('\n', '').split(separator)
+                    for info in standard:
+                        if info != '' and info in code: times += 1
+
+                if times == len(standard):
+                    print('发现面向用例代码——' + code, end=' ')
+                    cheats += 1  # 匹配成功
+                    print('当前发现作弊次数' + str(cheats))
+        return cheats / len(cases)
--- evaluator.py
+++ evaluator.py
@@ -3,93 +3,84 @@ import zipfile
 import os
 import time
 import json
+from defender import Defender
 
 
 class ScoreEvaluator:
-    workdir = os.path.abspath('.')
+    work_dir = os.path.abspath('.')
     cases = {}
     lines = 0
+    all_the_code = []
 
     @classmethod
-    def read_from_url(cls, url):
-        request.urlretrieve(url, 'code.zip')
+    # src_url 代码包url
+    def read_from_url(cls, src_url):
+        request.urlretrieve(src_url, 'code.zip')
         src = zipfile.ZipFile('code.zip')
         src.extractall('tmp')
-        dir = 'tmp/'+src.namelist()[0]
-        files = zipfile.ZipFile(dir)  # 解压完成
-
-        iscpp = False
-        for f in files.namelist():
-            if f == 'main.cpp':
-                iscpp = True
-                break
-        if iscpp: return None  # 判断是否为cpp提交
-
-        files.extractall(cls.workdir+'/resource')
-
+        tmp_dir = 'tmp/' + src.namelist()[0]
+        files = zipfile.ZipFile(tmp_dir)  # 解压完成
+        files.extractall(cls.work_dir + '/resource')
         # 这边的代码是用来处理后事的
-        os.system('rm -rf {}'.format(cls.workdir + '/tmp'))  # 删除非空空目录，这个命令很危险路径千万别填错！！！
-        os.system('rm {}'.format(cls.workdir + '/code.zip'))  # 删除zip文件
+        os.system('rm -rf {}'.format(cls.work_dir + '/tmp'))  # 删除非空空目录，这个命令很危险路径不能错
+        os.system('rm {}'.format(cls.work_dir + '/code.zip'))  # 删除zip文件
         # 这里是用的macOS终端命令 不一定适用于Windows cmd，如果报错改成相应的cmd命令即可
 
-        return cls.workdir+'/resource/main.py'  # resource文件夹存放爬取来的、已解压的文件资源
+        return cls.work_dir + '/resource/main.py'  # resource文件夹存放爬取来的、已解压的文件资源
 
     @classmethod
-    # TODO 逻辑仍需要修改！还需要匹配input！
-    def is_cheated(cls, file, separator):
-        test_cases = cls.workdir + '/resource/.mooctest/testCases.json'
-        all_the_code = open(file).read().split('\n')
-        cls.lines = len(all_the_code)
-        with open (test_cases, 'r') as f:
+    # 提取文件中的信息
+    def load(cls, file):
+        test_cases = cls.work_dir + '/resource/.mooctest/testCases.json'
+        cls.all_the_code = open(file).read().split('\n')
+        cls.lines = len(cls.all_the_code)
+        with open(test_cases, 'r') as f:
             cls.cases = json.load(f)
-        cheats = 0
-        for case in cls.cases:
-            output = case["output"]
-            if output != 'True' and output != 'False' and output != 'true' and output != 'false':
-                output = output.replace('\n', '').split(separator)
-                for code in all_the_code:
-                    code = code.replace('#.*', '')
-                    if code != '' and 'return' not in code:
-                        times = 0
-                        for out in output:
-                            if out in code: times += 1
-                        if times == len(output): cheats += 1  # 匹配成功
-                # 这边的代码逻辑有待商榷 到底是只要匹配到一个用例就算作弊 还是匹配到所有才算作弊
-        if cheats: return 1-cheats/len(cls.cases)
-        return False
+
     # 返回值是一个元组
     @classmethod
-    def getscore(cls, code_url, recycle=5, separator=' '):
+    # 循环次数recycle，这是为了减少os.system运行python导致的时间误差，默认为5
+    # 测试用例输出的分割符，默认为空格
+    def getScore(cls, code_url, recycle=5, separator=' '):
         file = cls.read_from_url(code_url)
-        if not file:
-            os.system('rm -rf {}'.format(cls.workdir + '/resource'))
-            return -1, cls.lines  # cpp提交
-        cheats = cls.is_cheated(file, separator)
-        if cheats:
-            os.system('rm -rf {}'.format(cls.workdir + '/resource'))
-            return cheats, cls.lines  # 面向用例返回面向用例占比，0到1之间
+        ScoreEvaluator.load(file)  # 导入文件，提取信息
 
-        runtime = 0
+        # cpp提交，需要修改
+        if Defender.cppDefend(cls.all_the_code):
+            os.system('rm -rf {}'.format(cls.work_dir + '/resource'))
+            return -1, cls.lines  # cpp提交
+
+        # 作弊代码
+        cheats = Defender.cheatDefend(separator, cls.all_the_code, cls.cases)
+        if cheats > 0:
+            os.system('rm -rf {}'.format(cls.work_dir + '/resource'))
+            return 1 - cheats, cls.lines  # 面向用例返回面向用例占比，0到1之间
+
+        runtime = 0  # 运行时间
         for i in range(recycle):
             for case in cls.cases:
-                input = case["input"] + '\n'
-                test = open(cls.workdir+'/test.txt', 'w')
-                test.write(input)
+                inputs = case["input"] + '\n'
+                test = open(cls.work_dir + '/test.txt', 'w')
+                test.write(inputs)
                 test.close()
                 timestamp_start = time.time() * 1000
-                os.system('python3 {}<{}>>{}'.format(file, cls.workdir+'/test.txt', cls.workdir+'/test.txt'))  # 与真实的运行时间有略微差异，因为是调用os模块从命令行调用的
+                os.system('python3 {}<{}>>{}'.format(file, cls.work_dir + '/test.txt',
+                                                     cls.work_dir + '/test.txt'))  # 与真实的运行时间有略微差异，因为是调用os模块从命令行调用的
                 timestamp_end = time.time() * 1000
                 runtime += timestamp_end - timestamp_start
         runtime /= recycle  # 通过取平均值尽量减少os.system带来的时间波动误差，如果对次数不满意可以自己传入recycle参数
         # 确保配置了python的环境变量
         # window环境下将上面的"python3"修改成"python"
 
-        os.system('rm {}'.format(cls.workdir+'/test.txt'))
-        os.system('rm -rf {}'.format(cls.workdir + '/resource'))  # 完成分析后，删除下载下来的资源
+        os.system('rm {}'.format(cls.work_dir + '/test.txt'))
+        os.system('rm -rf {}'.format(cls.work_dir + '/resource'))  # 完成分析后，删除下载下来的资源
         return runtime, cls.lines  # 暂时不知道怎么评分 还是先就返回个运行时间吧
 
 
+# 程序入口
 if __name__ == '__main__':
-    print('Analyzing···')
+    print('开始分析···')
+    print('请输入提交代码url：', end='')
     url = input()
-    print(ScoreEvaluator.getscore(url))
+    print(ScoreEvaluator.getScore(url))
+
